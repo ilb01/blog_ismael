73e23fb6b822697e5a422c4907702d9e1186ff0a
ilb01 <ilb01@iesemilidarder.com>
2025-01-28 20:47:26 +0100

    laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
+
         $this->call(CategorySeeder::class);
 
         $adminUser = new User();
@@ -24,10 +25,9 @@ class DatabaseSeeder extends Seeder
         $adminUser->role = "superadmin";
         $adminUser->password = Hash::make(12345678);
         $adminUser->save();
-        /*User::factory()->create([
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        ]);*/
+
+        User::factory(10)->create();
+        Post::factory(50)->create();
     }
 }
 
